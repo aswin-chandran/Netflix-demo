ab1448146385afa05ef43242208bb6929b7b5225
aswinchandran <aj98ply@gmail.com>
2023-07-04 22:02:23 +0530

    video player added

--- src/Components/Rowpost/Rowpost.js
+++ src/Components/Rowpost/Rowpost.js
@@ -1,32 +1,60 @@
-import React,{useEffect,useState} from 'react'
-import './Rowpost.css'
-import {imageUrl } from '../../constants/constants'
-import axios from '../../axios'
+import React, { useEffect, useState } from "react";
+import "./Rowpost.css";
+import { imageUrl,API_KEY } from "../../constants/constants";
+import axios from "../../axios";
+import Youtube from 'react-youtube'
 
 function Rowpost(props) {
-  const [movies,setMovies]=useState([])
-  useEffect(()=>{
-axios.get(props.url).then(response=>{
-  console.log(response.data)
-  setMovies(response.data.results)
-}).catch(err=>{
-  // alert('error');
+  const [movies, setMovies] = useState([]);
+  const [urlId,setUrlId]=useState('')
+  useEffect(() => 
+  
+  {
+    axios
+      .get(props.url)
+      .then((response) => {
+        console.log(response.data);
+        setMovies(response.data.results);
+      })
+      .catch((err) => {
+        // alert('error');
+      });
+  }, [props]);
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 0,
+    },
+  };
+  const handleMovie=(id)=>{
+console.log(id)
+axios.get(`/movie/${id}}/videos?api_key=${API_KEY}`).then(response=>{
+  if(response.data.results.length!==0){
+    setUrlId(response.data.results[0])
+  }
+  else{
+    console.log('trailer not available')
+  }
 })
-  },[props])
+  }
   return (
- 
-    <div className='row'>
+    <div className="row">
       <h2>{props.title}</h2>
       <div className="posters">
-        {movies.map((obj)=>
-          <img className={props.isSmall?'smallPoster':'poster'} src={`${imageUrl+obj.backdrop_path}`} alt="poster" />
-
-        )}
-
-
+        {movies.map((obj) => (
+          <img
+            onClick={()=>handleMovie(obj.id)}
+            className={props.isSmall ? "smallPoster" : "poster"}
+            src={`${imageUrl + obj.backdrop_path}`}
+            alt="poster"
+          />
+        ))}
       </div>
+    {urlId && <Youtube opts={opts} videoId={urlId.key}/>}
     </div>
-  )
+  );
 }
 
-export default Rowpost
+export default Rowpost;
