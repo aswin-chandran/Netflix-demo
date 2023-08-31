e2e5bc0de8427b4f37fcbcf16c50b712a9b2168f
aswin chandran <aswin1998ply@gmail.com>
2023-08-31 15:10:49 +0530

    add r and t

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Banner from './Components/Banner/Banner';
 import NavBar from './Components/NavBar/NavBar';
 import Rowpost from './Components/Rowpost/Rowpost';
-import {action,originals} from './urls'
+import {action,originals,romance,horror,trending} from './urls'
 
 
 
@@ -15,7 +15,13 @@ function App() {
      <NavBar/>
      <Banner/>
      <Rowpost url={originals}title='Netflix Originals'/>
+     <Rowpost url={trending}title='Trending' isSmall />
      <Rowpost url={action}title='Action' isSmall />
+     <Rowpost url={romance}title='Romance' isSmall />
+     <Rowpost url={horror}title='Horror' isSmall />
+
+      
+      
     
     
 
--- src/urls.js
+++ src/urls.js
@@ -1,3 +1,6 @@
 import {API_KEY} from './constants/constants'
 export const originals=`discover/tv?api_key=${API_KEY}&with_networks=213`
 export const action=`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=28`
+export const romance = `/discover/movie?api_key=${API_KEY}&with_genres=10749` 
+export const horror = `/discover/movie?api_key=${API_KEY}&with_genres=27` 
+export const trending = `/trending/all/week?api_key=${API_KEY}` 
